88b12417c0f87d35e461032efd475234da010582
CaioCeretta <caioceretta@gmail.com>
2025-05-27 00:45:27 -0300

    chore: Update export and import

--- mentoria/specialist/Next.Js/tic-tac-toe/packages/core/src/index.ts
+++ mentoria/specialist/Next.Js/tic-tac-toe/packages/core/src/index.ts
@@ -1,5 +1,7 @@
 import Player from './player/Player'
 import { PlayerType } from './shared/PlayerType'
 import Cell from './shared/Cell'
+import GameResult from './result/GameResult'
+import Board from './game/Board'
 
-export { Player, Cell, PlayerType }
+export { Player, Cell, PlayerType, GameResult, Board }
