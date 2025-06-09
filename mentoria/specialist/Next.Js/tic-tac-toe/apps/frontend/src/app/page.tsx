2d686fc0c4856c9712dd07ac51e5bbc89bb6032e
CaioCeretta <caioceretta@gmail.com>
2025-06-09 14:32:12 -0300

    feat:  Game components

--- mentoria/specialist/Next.Js/tic-tac-toe/apps/frontend/src/app/page.tsx
+++ mentoria/specialist/Next.Js/tic-tac-toe/apps/frontend/src/app/page.tsx
@@ -1,11 +1,12 @@
-import Card from '@/components/shared/Card'
+import CellArea from "@/components/game/CellArea";
+import { PlayerType } from "core";
 
 export default function Home() {
-	return (
-		<div>
-			<Card color='primary'>
-				Estou dentro de um card
-			</Card>
-		</div>
-	)
+  return (
+    <div>
+      <CellArea type={PlayerType.X} selected />
+      <CellArea type={PlayerType.O} selected />
+      <CellArea />
+    </div>
+  );
 }
