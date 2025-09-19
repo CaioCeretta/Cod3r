5638cd9c64b92ed500e506ca4e2be33883d82350
CaioCeretta <caioceretta@gmail.com>
2025-09-18 21:40:59 -0300

    feat: useTamanhoJanela hook

--- mentoria/specialist/Trilha - React & Next/hooks-inicial/src/pages/personalizados/modal.tsx
+++ mentoria/specialist/Trilha - React & Next/hooks-inicial/src/pages/personalizados/modal.tsx
@@ -2,22 +2,37 @@ import { IconX } from "@tabler/icons-react";
 import Botao from "@/components/template/Botao";
 import Flex from "@/components/template/Flex";
 import Pagina from "@/components/template/Pagina";
+import useTamanhoJanela from "@/data/hooks/useTamanhoJanela";
 import useToggle from "@/data/hooks/useToggle";
 
 export default function () {
-  const [mostrar, toggleMostrar] = useToggle(false)
+  const [mostrar, toggleMostrar] = useToggle(false);
+  const [tamanho] = useTamanhoJanela();
 
   return (
-    <Pagina titulo="Modal" subtitulo="Usando um hook personalizado para abrir e fechar uma modal">
+    <Pagina
+      titulo="Modal"
+      subtitulo="Usando um hook personalizado para abrir e fechar uma modal"
+    >
       <Botao texto="Abrir" cor="bg-cyan-500" onClick={toggleMostrar} />
       {mostrar ? (
-
-        <Flex center col
+        <Flex
+          center
+          col
           className={`w-80 h-80 bg-gray-500 text-white text-xl
-                          p-4 m-4 rounded-md`}>
-          <Botao cor="bg-red-700" tamanho="xl" onClick={toggleMostrar} icone={<IconX />} />
+                          p-4 m-4 rounded-md`}
+        >
+          <Botao
+            cor="bg-red-700"
+            tamanho="xl"
+            onClick={toggleMostrar}
+            icone={<IconX />}
+          />
           <h1 className="mt-5">Clique em X para sair</h1>
-        </Flex>) : ""}
+        </Flex>
+      ) : (
+        ""
+      )}
     </Pagina>
-  )
+  );
 }
