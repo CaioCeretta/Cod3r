f97d6b01fe820c8357c88c32f00f4f3903318a9a
CaioCeretta <caioceretta@gmail.com>
2025-09-29 23:56:52 -0300

    feat: applying the TemaProvider in our app

--- specialist/Trilha - React & Next/React Hooks/src/components/template/Cabecalho.tsx
+++ specialist/Trilha - React & Next/React Hooks/src/components/template/Cabecalho.tsx
@@ -1,4 +1,7 @@
+import { IconPaint, IconX } from "@tabler/icons-react";
+import useToggle from "@/data/hooks/useToggle";
 import Flex from "./Flex";
+import SeletorDeCor from "./SeletorDeCor";
 import Titulo from "./Titulo";
 
 interface CabecalhoProps {
@@ -7,9 +10,21 @@ interface CabecalhoProps {
 }
 
 export default function Cabecalho(props: CabecalhoProps) {
+    const [mostraSeletor, toggleMostraSeletor] = useToggle(false);
+
     return (
         <Flex centerCross className="justify-between">
-            {props.titulo ? <Titulo titulo={props.titulo} subtitulo={props.subtitulo} /> :<div></div> }
+            {props.titulo ? (
+                <Titulo titulo={props.titulo} subtitulo={props.subtitulo} />
+            ) : (
+                <div></div>
+            )}
+            <div>
+                <button onClick={toggleMostraSeletor} type="button">
+                    {mostraSeletor ? <IconX /> : <IconPaint />}
+                </button>
+            </div>
+            {mostraSeletor ? <SeletorDeCor></SeletorDeCor> : ""}
         </Flex>
     );
 }
