73c7236ba0fff2d4db73e281760517b127eb6885
CaioCeretta <caioceretta@gmail.com>
2025-10-07 23:13:58 -0300

    fix: Fixed hydration error

--- specialist/Trilha - React & Next/React Hooks/src/components/template/MenuPrincipalSecao.tsx
+++ specialist/Trilha - React & Next/React Hooks/src/components/template/MenuPrincipalSecao.tsx
@@ -1,7 +1,4 @@
-/** biome-ignore-all lint/a11y/useKeyWithClickEvents: <explanation> */
 import { IconMinus, IconPlus } from "@tabler/icons-react";
-import useBoolean from "@/data/hooks/useBoolean";
-import useToggle from "@/data/hooks/useToggle";
 import Flex from "./Flex";
 
 interface MenuSecaoProps {
@@ -15,28 +12,38 @@ interface MenuSecaoProps {
 export default function MenuSecao(props: MenuSecaoProps) {
     const { titulo, mini, aberta } = props;
 
+    // CSS classes on separate variable
+    const buttonClasses = `
+        flex items-center justify-between
+        text-zinc-400 uppercase font-bold 
+        ${mini && "text-[11px]"} cursor-pointer
+        w-full text-left // Importante para o botão preencher o espaço e alinhar o texto
+    `;
 
     return (
         <Flex col gap={4} className={`${mini && "items-center"}`}>
+            {/* 1. Button for the clickable area */}
             <button
                 type="button"
-                className={`
-                flex items-center justify-between
-                text-zinc-400 uppercase font-bold 
-                ${mini && "text-[11px]"} cursor-pointer
-            `} onClick={() => props.onClick?.()}
+                className={buttonClasses}
+                onClick={props.onClick} // onClick on the outer button
+                aria-expanded={aberta} // Acessibility property to describe that the button is expanded
             >
+                {/* 2. Button content, title and icon */}
                 {mini ? (
                     titulo
                 ) : (
                     <>
-                        {titulo}
-                        <button onClick={() => props.onClick?.()} type="button">
-                            {aberta ? <IconMinus size={15} /> : <IconPlus size={15} />}
-                        </button>
+                        {/* Title */}
+                        <span>{titulo}</span>
+
+                        {/* Icon as visual part of the button */}
+                        {aberta ? <IconMinus size={15} /> : <IconPlus size={15} />}
                     </>
                 )}
             </button>
+
+            {/* Rest of content */}
             {aberta && (
                 <Flex col gap={1.5}>
                     {props.children}
