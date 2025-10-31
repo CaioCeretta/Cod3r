64c4f825c6e26c762565de170866e268b24b6624
CaioCeretta <caioceretta@gmail.com>
2025-10-31 00:04:20 -0300

    feat: buscarPorEmail

--- architect/Software Design/clean-hexagonal-arch/architecture-code/src/exemplo/adaptadores/db/UsuarioEmMemoria.ts
+++ architect/Software Design/clean-hexagonal-arch/architecture-code/src/exemplo/adaptadores/db/UsuarioEmMemoria.ts
@@ -4,7 +4,21 @@ import type Usuario from "../../app/usuario/Usuario";
 export default class UsuarioEmMemoria implements ColecaoUsuario {
 	private static itens: Usuario[] = [];
 
+	async buscarPorEmail(email: string): Promise<Usuario | null> {
+		const usuario = UsuarioEmMemoria.itens.find(
+			(usuario) => usuario.email === email,
+		);
+
+		return usuario ?? null;
+	}
+
 	async inserir(item: Usuario): Promise<void> {
 		UsuarioEmMemoria.itens.push(item);
+
+		return Promise.resolve();
+	}
+
+	static resetar() {
+		UsuarioEmMemoria.itens = [];
 	}
 }
