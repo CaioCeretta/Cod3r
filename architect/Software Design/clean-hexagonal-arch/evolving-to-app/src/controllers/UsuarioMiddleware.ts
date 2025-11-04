c462b89a50083272f3fe9f01d92cb21e6187f556
CaioCeretta <caioceretta@gmail.com>
2025-11-03 23:59:51 -0300

    feat: ensure authenticated user middleware

---
+++ architect/Software Design/clean-hexagonal-arch/evolving-to-app/src/controllers/UsuarioMiddleware.ts
@@ -0,0 +1,36 @@
+import type { NextFunction, Request, Response } from "express";
+import type ColecaoUsuario from "../core/usuario/ColecaoUsuario";
+import type ProvedorToken from "../core/usuario/ProvedorToken";
+import type Usuario from "../core/usuario/Usuario";
+
+export default function UsuarioMiddleware(
+	colecao: ColecaoUsuario,
+	provedorToken: ProvedorToken,
+) {
+	return async (req: Request, res: Response, next: NextFunction) => {
+		const token = req.headers.authorization.replace("Bearer", "");
+		const acessoNegado = () => res.status(403).send("Token Invalido");
+
+		try {
+			if (!token) {
+				acessoNegado();
+				return;
+			}
+
+			const usuarioToken = provedorToken.validar(token) as Usuario;
+
+			const usuario = await colecao.buscarPorEmail(usuarioToken.email);
+
+			if (!usuario) {
+				acessoNegado();
+				return;
+			}
+
+			(req as any).usuario = usuario;
+
+			next();
+		} catch (err) {
+			acessoNegado();
+		}
+	};
+}
