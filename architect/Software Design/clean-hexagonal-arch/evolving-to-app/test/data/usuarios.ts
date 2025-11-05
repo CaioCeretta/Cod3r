7ce02426c4de08d9894972fcc9fa9c57d831b8aa
CaioCeretta <caioceretta@gmail.com>
2025-11-05 00:44:50 -0300

    chore: define constant to be used across tests

---
+++ architect/Software Design/clean-hexagonal-arch/evolving-to-app/test/data/usuarios.ts
@@ -0,0 +1,10 @@
+import type Usuario from "../../src/core/usuario/Usuario";
+
+export default {
+	completo: {
+		id: "0c7fa33f-a966-4e2b-96c1-13ffea75b1c7",
+		nome: "Caio",
+		email: "ccc@zmail.com.br",
+		senha: "123456",
+	} as Usuario,
+};
