a9c3e74cd7fc67a94b8b82e0be58e04ada68ce0e
CaioCeretta <caioceretta@gmail.com>
2025-11-07 01:56:33 -0300

    chore: add transaction list arrays

--- architect/Software Design/clean-hexagonal-arch/evolving-to-app/test/data/transacoes.ts
+++ architect/Software Design/clean-hexagonal-arch/evolving-to-app/test/data/transacoes.ts
@@ -1,18 +1,26 @@
 import type Transacao from "../../src/core/transacao/Transacao";
 
-export default {
-	completa: {
-		id: "1",
-		descricao: "Comida",
-		valor: -100,
-		vencimento: new Date("2025-10-10"),
-		idUsuario: "7ae32a56-57b7-417a-a2de-8f6d496139da",
-	} as Transacao,
+const transacaoRef = {
+	descricao: "Comida",
+	valor: -100,
+	vencimento: new Date("2025-10-10"),
+	idUsuario: "7ae32a56-57b7-417a-a2de-8f6d496139da",
+} as Transacao;
 
-	semId: {
-		descricao: "Comida",
-		valor: -100,
-		vencimento: new Date("2025-10-10"),
-		idUsuario: "7ae32a56-57b7-417a-a2de-8f6d496139da",
-	} as Transacao,
+const transacaoRef2 = {
+	id: "1",
+	descricao: "Comida",
+	valor: -100,
+	vencimento: new Date("2025-10-10"),
+	idUsuario: "7ae32a56-57b7-417a-a2de-8f6d496139da",
+} as Transacao;
+
+export default {
+	semId: transacaoRef,
+	lista: [
+		{ ...transacaoRef, valor: 3000, descricao: "Salário" },
+		{ ...transacaoRef, valor: -450, descricao: "Conta de Luz" },
+		{ ...transacaoRef, valor: -100, descricao: "Conta de Água" },
+		{ ...transacaoRef, valor: -200, descricao: "Conta de Internet" },
+	],
 };
