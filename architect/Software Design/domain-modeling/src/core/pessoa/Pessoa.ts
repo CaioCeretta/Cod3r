b2f166a3216a2f25860ba4c867e6f795977d0a6f
CaioCeretta <caioceretta@gmail.com>
2025-11-09 01:10:47 -0300

    feat: pessoa class

---
+++ architect/Software Design/domain-modeling/src/core/pessoa/Pessoa.ts
@@ -0,0 +1,23 @@
+import Cpf from "../shared/Cpf";
+import Id from "../shared/Id";
+import NomePessoa from "../shared/NomePessoa";
+
+export interface PessoaProps {
+	id?: string;
+	nome?: string;
+	cpf?: string;
+}
+
+export default class Pessoa {
+	readonly props: PessoaProps;
+	readonly id: Id;
+	readonly nome: NomePessoa;
+	readonly cpf: Cpf;
+
+	constructor(props: PessoaProps) {
+		this.props = props;
+		this.nome = new NomePessoa(props.nome);
+		this.id = new Id(props.id);
+		this.cpf = new Cpf(props.cpf);
+	}
+}
