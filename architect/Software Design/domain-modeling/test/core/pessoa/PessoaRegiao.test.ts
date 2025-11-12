6b35208539ae2e5bffe82a7fda39665b1a4471ec
CaioCeretta <caioceretta@gmail.com>
2025-11-12 00:08:11 -0300

    Final Tests

---
+++ architect/Software Design/domain-modeling/test/core/pessoa/PessoaRegiao.test.ts
@@ -0,0 +1,20 @@
+import PessoasPorRegiao from "@/core/pessoa/PessoasPorRegiao";
+import RegiaoCpf from "@/core/shared/RegiaoCpf";
+import PessoaBuilder from "@/test/data/PessoaBuilder";
+
+test("Deve agrupar as pessoas da região de SP", () => {
+	const pessoas = PessoaBuilder.criarLista(10000);
+	const agrupadas = new PessoasPorRegiao(pessoas).agrupar();
+
+	const pessoasSP = agrupadas.get(RegiaoCpf.SP) ?? [];
+	const pessoasRJ = agrupadas.get(RegiaoCpf.ES_RJ) ?? [];
+	const mesmaRegiaoSP = pessoasSP.every((p) =>
+		p.cpf.regiao.igual(RegiaoCpf.SP),
+	);
+	const regiaoDiff = pessoasRJ.every((p) =>
+		p.cpf.regiao.diferente(RegiaoCpf.SP),
+	);
+
+	expect(mesmaRegiaoSP).toBe(true);
+	expect(regiaoDiff).toBe(true);
+});
