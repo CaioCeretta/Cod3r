7c26f8df1d6645aab25e07bbb793a8394f6fe606
CaioCeretta <caioceretta@gmail.com>
2025-11-16 10:29:14 -0300

    chore: CursoBuilder

---
+++ architect/Software Design/domain-modeling/ead/test/data/CursoBuilder.ts
@@ -0,0 +1,73 @@
+import { faker } from "@faker-js/faker";
+import type Capitulo from "@/curso/Capítulo";
+import type { CursoProps } from "@/curso/Curso";
+import Curso from "@/curso/Curso";
+import CapituloBuilder from "./CapituloBuilder";
+import NomesCurso from "./NomesCurso";
+
+export default class CursoBuilder {
+	private constructor(private props: CursoProps) {}
+
+	static criar(qtdeCapitulos: number = 10, qtdeAulas: number = 10) {
+		return new CursoBuilder({
+			nome: NomesCurso.aleatorio(),
+			data: faker.date.recent(),
+			capitulos: CapituloBuilder.criarListaCom(qtdeCapitulos, qtdeAulas).map(
+				(c) => c.props,
+			),
+		});
+	}
+	comId(id: string): CursoBuilder {
+		this.props.id = id;
+		return this;
+	}
+
+	semId(): CursoBuilder {
+		this.props.id = undefined;
+		return this;
+	}
+
+	comNome(nome: string): CursoBuilder {
+		this.props.nome = nome;
+		return this;
+	}
+
+	semNome(): CursoBuilder {
+		this.props.nome = undefined;
+		return this;
+	}
+
+	comDuracao(duracao: number): CursoBuilder {
+		this.props.duracao = duracao;
+		return this;
+	}
+
+	semDuracao(): CursoBuilder {
+		this.props.duracao = undefined;
+		return this;
+	}
+
+	comQuantidadeDeAulas(quantidadeDeAulas: number): CursoBuilder {
+		this.props.quantidadeDeAulas = quantidadeDeAulas;
+		return this;
+	}
+
+	semQuantidadeDeAulas(): CursoBuilder {
+		this.props.quantidadeDeAulas = undefined;
+		return this;
+	}
+
+	comCapitulos(capitulos: Capitulo[]): CursoBuilder {
+		this.props.capitulos = capitulos.map((a) => a.props);
+		return this;
+	}
+
+	semCapitulos(): CursoBuilder {
+		this.props.capitulos = undefined;
+		return this;
+	}
+
+	agora(): Curso {
+		return new Curso(this.props);
+	}
+}
